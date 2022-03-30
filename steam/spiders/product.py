7b59905fb793c715309c7dab42fa063d075a8a6d
FOREVEREALIZE <60579848+FOREVEREALIZE@users.noreply.github.com>
2022-03-30 20:00:35 +0200

    Spider

---
+++ steam/spiders/product.py
@@ -0,0 +1,25 @@
+class ProductSpider(CrawlSpider):
+    name = 'products'
+    start_urls = ["http://store.steampowered.com/search/?sort_by=Released_DESC"]
+    allowed_domains=["steampowered.com"]
+    rules = [
+        Rule(
+            LinkExtractor(
+                allow='/app/(.+)/',
+                restrict_css='#search_result_container'
+            ),
+            callback='parse_product'
+        ),
+        Rule(
+            LinkExtractor(
+                allow='page=(d+)',
+                restrict_css='.search_pagination_right'
+            )
+        )
+    ]
+
+    def parse_product(self, response):
+        return {
+            'app_name': response.css('.apphub_AppName ::text').extract_first(),
+            'specs': response.css('.game_area_details_specs a ::text').extract()
+        }
