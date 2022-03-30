1dc5d7bf68d603d16eecabae67023cd681d0b63d
FOREVEREALIZE <60579848+FOREVEREALIZE@users.noreply.github.com>
2022-03-30 20:03:58 +0200

    Fix the Fix that fixed the Fix that fixed the Fix

--- steam/spiders/product.py
+++ steam/spiders/product.py
@@ -1,6 +1,6 @@
 import scrapy
 
-class ProductSpider(scrapy.spider.CrawlSpider):
+class ProductSpider(scrapy.spiders.CrawlSpider):
     name = 'products'
     start_urls = ["http://store.steampowered.com/search/?sort_by=Released_DESC"]
     allowed_domains=["steampowered.com"]
